96b7fdaabb7f7f18d926fb5b87536c3e999ce8d2
dhany007 <dhanyaritonang@gmail.com>
2019-10-21 03:16:30 +0700

    Database configuration

--- app.js
+++ app.js
@@ -2,9 +2,33 @@ require('dotenv/config')
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mysql = require('mysql')
 
 const app = express()
 const port = process.env.port || 3000
+
+const conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+conn.connect((err) => {
+    if (err) {
+        console.log(`Error : ${err}\n`)
+    } else {
+        console.log('\nDatabase connected!\n')
+    }
+})
+
+conn.end((err) => {
+    if (err) {
+        console.log(`Error : ${err}\n`)
+    } else {
+        console.log('\nDatabase closed!')
+    }
+})
 
 app.listen(port, () => {
     console.log(`\nServer listening on port ${port}`)
